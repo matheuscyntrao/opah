2fd3cb17d0be9939de2d0899d66a255c4f687aeb
Cyntrão <matheus.cyntrao@gmail.com>
2025-04-05 15:11:15 -0300

    Adicionando capabilities a partir do modelo W3C para rodar o app android nativo no UiAutomator2.

---
+++ android_cucumber_androidstudiodevice_specreporter_appium.conf.ts
@@ -0,0 +1,36 @@
+exports.config = {
+    runner: 'local',
+    specs: ['./features/**/*.feature'], // Caminho para seus arquivos .feature
+    capabilities: [{
+        platformName: 'Android',
+        browserName: 'Chromedriver',
+        'appium:deviceName': 'Medium Phone API 36',
+        'appium:automationName': 'UiAutomator2',
+        'appium:app' : '/wdionative.apk',
+        'appium:printPageSourceO' : true
+  
+    }],
+    services: ['appium'],
+    framework: 'cucumber',
+    cucumberOpts: {
+      require: ['./features/step-definitions/*.ts'], // Caminho para seus arquivos de steps
+      backtrace: false,
+      requireModule: [],
+      dryRun: false,
+      failFast: false,
+      snippets: true,
+      source: true,
+      strict: false,
+      tagExpression: '',
+      timeout: 60000,
+      ignoreUndefinedDefinitions: false,
+    },
+    reporters: ['spec'],
+    appium: {
+      command: 'appium',
+      args: {
+        address: '127.0.0.1',
+        port: 4723,
+      },
+    },
+  };
